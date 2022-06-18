0b61eb2f8a86605e7a2a644ff80ab0cd5d7e067d
weirui88888 <ruiwei88888@163.com>
2022-06-18 15:51:36 +0800

    :bulb:Support hand-painted Chinese style

--- src/actions/actionProperties.tsx
+++ src/actions/actionProperties.tsx
@@ -33,6 +33,7 @@ import {
   TextAlignTopIcon,
   TextAlignBottomIcon,
   TextAlignMiddleIcon,
+  FontFamilySHIcon,
 } from "../components/icons";
 import {
   DEFAULT_FONT_FAMILY,
@@ -669,6 +670,11 @@ export const actionChangeFontFamily = register({
         value: FONT_FAMILY.Cascadia,
         text: t("labels.code"),
         icon: <FontFamilyCodeIcon theme={appState.theme} />,
+      },
+      {
+        value: FONT_FAMILY.Muyao,
+        text: "手绘中文体",
+        icon: <FontFamilySHIcon theme={appState.theme} />,
       },
     ];
 
